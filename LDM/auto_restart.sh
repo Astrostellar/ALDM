504a1db50b864faf2064c73732047bd9a8fb906c
Astrostellar <szy469350797@gmail.com>
2025-06-07 08:09:51 +0000

    modify training for ldm

---
+++ LDM/auto_restart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 设置日志路径和时间戳
+LOG_FILE="run_log.txt"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+# 无限循环
+while true; do
+    echo "[$TIMESTAMP] Starting the Python script..." | tee -a "$LOG_FILE"
+    fuser -vk /dev/nvidia*
+    # 运行 Python 脚本
+    python main.py -b configs/latent-diffusion/brats-ldm-vq-4.yaml \
+                   -t True \
+                   --resume logs/2025-05-26T12-17-02_brats-ldm-vq-4/checkpoints/last.ckpt \
+                   --gpus 0,1,2,3
+
+    # 脚本异常退出后记录时间
+    TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "[$TIMESTAMP] Script crashed or exited. Restarting after 10 seconds..." | tee -a "$LOG_FILE"
+    
+    # 延迟重启时间
+    sleep 10
+done
